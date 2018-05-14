544b68b9309bdd2f630b403f7df402c947e8d025
smasuzawa <koubousei@gmail.com>
2018-05-14 16:41:58 +0900

    カテゴリーページ SP

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -59,3 +59,64 @@ $(window).on('load', function () {
 		return false;
 	});
 });
+
+
+
+$(document).ready(function() {
+	// スクロール時 アニメーション表示
+	$(window).scroll(function(){
+		var windowH = $(window).height();
+		var windowT = $(window).scrollTop();
+		$('.animV').each(function(){
+			var targetPos = $(this).offset().top;
+			if( windowT > targetPos - windowH + 100){
+				$(this).addClass('vertical');
+			}
+		});
+		$('.animH').each(function(){
+			var targetPos = $(this).offset().top;
+			if( windowT > targetPos - windowH + 60){
+				$(this).addClass('horizontal');
+			}
+		});
+	});
+
+	// ヘッダー固定によるリンク位置の調整
+	var windowWidth = $(window).width();
+	var headerHight;
+	if (windowWidth <= 750) {
+		headerHight = 65;
+	} else {
+		headerHight = 90;
+	}
+	$('a[href^=#]').click(function(){
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top-headerHight;
+		$("html, body").animate({scrollTop:position}, 550, "swing");
+		return false;
+	});
+});
+
+var windowWidth = $(window).width();
+var headerHight;
+if (windowWidth <= 750) {
+	headerHight = 65;
+} else {
+	headerHight = 90;
+}
+
+
+
+//カテゴリーページなどにあるソート
+$(function(){
+	$(".u-cateCondition .btnSort a").click(function(){
+		$(this).toggleClass("show");
+		var thisSort = $(this).parents($(".u-cateCondition"));
+		thisSort.find(".sortList1").slideToggle();
+		return false;
+	});
+});
+
+
+
